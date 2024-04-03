8028be43809325663995442345775e5fbebefd1d
Valkorz <pivarcivittorio@gmail.com>
2024-04-03 10:40:51 -0300

    Updated class name.

--- com/root/Main.java
+++ com/root/Main.java
@@ -1,6 +1,8 @@
 package com.root;
 
-public abstract class mainClass{
+public abstract class Main{
+    
+    public static UserData ActiveUser;
     public static void main(String[] args){
         System.out.println("Test");
         byte[] b = new byte[256];
